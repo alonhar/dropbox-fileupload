25302dec7fa0c0cbb40eb042c86a80588bb67fa0
Alon Harel <alonhar@hotmail.com>
2016-06-11 23:51:14 +0000

    first

---
+++ dropbox-upload.js
@@ -0,0 +1,42 @@
+"use strict";
+
+var fs         = require('fs');
+var request    = require('request');
+var path = require('path');
+
+function uploadFile(localPath,remotePath,token,callback){
+
+
+var fileToUploadName = localPath.split('/');
+fileToUploadName = fileToUploadName[fileToUploadName.length-1];
+remotePath = path.normalize("/"+remotePath + "/" +fileToUploadName);
+fs.readFile(localPath, function read(err, data) {
+	if (err){
+		console.log(err);
+		return ;
+	}
+	request.put(
+		    'https://api-content.dropbox.com/1/files_put/auto' + remotePath,
+			{
+				      headers: { Authorization: 'Bearer ' + token  },
+					        body: data
+							    
+			}, function(err, httpResponse, bodymsg) {
+				if (err || bodymsg.indexOf("error")>-1) {
+					  
+							callback(bodymsg);
+							      
+				}else{
+					if (callback){
+					callback();
+					}
+	
+				}				    
+			}
+			  
+	);
+
+});
+}
+module.exports = uploadFile;
+
---
+++ upload-dropbox-cli.js
@@ -0,0 +1,21 @@
+"use strict";
+var token     = process.argv[4];
+var localPath  = process.argv[2];
+var remotePath = process.argv[3];
+var exeFile = process.argv[1].split('/');
+var uploadFile = require('./dropbox-upload');
+exeFile = exeFile[exeFile.length-1];
+if (!token || !localPath || !remotePath){
+		console.log("Error: node "+ exeFile + " "+ "<localFile> "+ "<remotePath> "+"<token>" );
+			return ;
+
+
+}
+uploadFile(localPath,remotePath,token,function(err){
+	if (err){
+		console.log(err);
+	}else{
+		console.log("file uploaded!!");
+	}
+});
+	
